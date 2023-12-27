33d3cdb13c25d3d653670e1f88d9133acdd22817
Thatminer102 <pranavnatarajan1@gmail.com>
2023-12-26 22:47:07 -0600

    chore: removing unused libraries

--- src/Components/AddUser/AddUser.tsx
+++ src/Components/AddUser/AddUser.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { GenshinCharacter } from '../../Types/Genshin';
 import UidInputModal from '../UidInputModal/UidInputModal';
-import ShortCharacterList from '../CharacterList/ShortCharacterList';
 import CharacterList from '../CharacterList/CharacterList';
 
 interface AddUserProps {
@@ -17,29 +16,29 @@ const AddUser = ({ themePreference }: AddUserProps) => {
     return (
         <div>
             {characterData.length > 0 ? <CharacterList characters={characterData} themePreference={themePreference} /> : <div className="fixed hero min-h-screen">
-                        <div className="hero-content text-center">
-                            <div>
-                                <h1 className="text-5xl font-bold">No Characters Found.</h1>
-                                <div className="flex flex-col w-full lg:flex-row py-6">
-                                    <label
-                                        htmlFor="uid_modal"
-                                        className="h-20 rounded-box btn"
-                                    >
-                                        Import Characters from UID
-                                    </label>
-                                    <UidInputModal
-                                        themePreference={themePreference}
-                                        characterData={characterData}
-                                        populateCharacters={populateCharacters}
-                                    />
-                                    <div className="divider lg:divider-horizontal">OR</div>
-                                    <div className="h-20 rounded-box btn">Import Characters from JSON</div>
-                                </div>
-                            </div>
+                <div className="hero-content text-center">
+                    <div>
+                        <h1 className="text-5xl font-bold">No Characters Found.</h1>
+                        <div className="flex flex-col w-full lg:flex-row py-6">
+                            <label
+                                htmlFor="uid_modal"
+                                className="h-20 rounded-box btn"
+                            >
+                                Import Characters from UID
+                            </label>
+                            <UidInputModal
+                                themePreference={themePreference}
+                                characterData={characterData}
+                                populateCharacters={populateCharacters}
+                            />
+                            <div className="divider lg:divider-horizontal">OR</div>
+                            <div className="h-20 rounded-box btn">Import Characters from JSON</div>
                         </div>
-                    </div>}
+                    </div>
                 </div>
-            );
+            </div>}
+        </div>
+    );
 };
 
-            export default AddUser;
+export default AddUser;
